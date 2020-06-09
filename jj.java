49aa6716694f4443c3760c2603247813883bd582
ghatevm <46521415+ghatevm@users.noreply.github.com>
2020-06-08 23:00:24 -0700

    Update jj.java

--- jj.java
+++ jj.java
@@ -3,6 +3,6 @@ public class jj {
 
    public static void main(String[] args) {
         
-      System.out.println("Sum of these numbers: ");
+      System.out.println("HELLO WORLD !!!: ");
    }
 }
